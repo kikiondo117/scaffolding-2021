f86110234a1962e7b25e84574282df535c133aa3
carlos alberto vera villegas <kikiondo@carlostekiMacBook-Pro.local>
2021-02-23 17:45:52 -0600

    Constants

--- src/containers/Home/meta/reducer.js
+++ src/containers/Home/meta/reducer.js
@@ -1,14 +1,21 @@
 import produce from 'immer';
 import * as constans from './constants';
 
-export const initialState = {};
+export const initialState = {
+  nani: '',
+};
 
 export const reducerName = 'Home';
 
 const HomeReducer = (state = initialState, action) =>
   produce(state, (draft) => {
     switch (action.type) {
+      case constans.TEST:
+        draft.nani = 'nani';
+        return null;
       default:
         break;
     }
   });
+
+export { HomeReducer };
